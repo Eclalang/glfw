18cf74b680895e89b8f274b485e4d0ce105e5dd7
paul chesneau <pchesneau3103@gmail.com>
2023-03-01 11:17:59 +0100

    refactor

--- v3.2/glfw/glfw/deps/dummy.go
+++ v3.2/glfw/glfw/deps/dummy.go
@@ -1,3 +1,4 @@
+//go:build required
 // +build required
 
 // Package dummy prevents go tooling from stripping the c dependencies.
@@ -5,8 +6,8 @@ package dummy
 
 import (
 	// Prevent go tooling from stripping out the c source files.
-	_ "github.com/go-gl/glfw/v3.2/glfw/glfw/deps/KHR"
-	_ "github.com/go-gl/glfw/v3.2/glfw/glfw/deps/glad"
-	_ "github.com/go-gl/glfw/v3.2/glfw/glfw/deps/mingw"
-	_ "github.com/go-gl/glfw/v3.2/glfw/glfw/deps/vulkan"
+	_ "github.com/Eclalang/glfw/v3.2/glfw/glfw/deps/KHR"
+	_ "github.com/Eclalang/glfw/v3.2/glfw/glfw/deps/glad"
+	_ "github.com/Eclalang/glfw/v3.2/glfw/glfw/deps/mingw"
+	_ "github.com/Eclalang/glfw/v3.2/glfw/glfw/deps/vulkan"
 )
